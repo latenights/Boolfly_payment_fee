9f7f631e94e988cd57249060b9cf61fe41dd6034
Erik Kalinak <ekalinak@gmail.com>
2018-08-08 22:56:00 +0200

    Fixed JS error in case shipping method was unset by some service.

--- view/frontend/web/js/action/payment/select-payment-method.js
+++ view/frontend/web/js/action/payment/select-payment-method.js
@@ -12,7 +12,9 @@ define(
 
         return function (paymentMethod) {
             quote.paymentMethod(paymentMethod);
-            totals(isLoading, paymentMethod['method']);
+			if (paymentMethod){
+            	totals(isLoading, paymentMethod['method']);
+			}
         }
     }
 );
